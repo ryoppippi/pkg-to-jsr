02f25e07d5f8984890a89278a2be5087c6ae0f74
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-06 16:45:08 +0100

    feat: update build process and dependencies
    The build process has been updated to use glob patterns for entrypoints
    instead of a single entry file. This allows for more flexibility in the
    build process.
    
    The 'bun-plugin-isolated-decl' and 'oxc-transform' dependencies have been
    added. These are used to improve the build process and output.
    
    The 'pkg-to-jsr' command now points to 'dist/bin.mjs' instead of
    'dist/index.mjs'. This change reflects the new build process.
    
    The 'bun.lockb' file has been updated to reflect the new dependencies.

--- build.ts
+++ build.ts
@@ -1,17 +1,23 @@
-import { $ } from 'bun';
+import path from 'node:path';
+import { $, Glob } from 'bun';
+import isolatedDecl from 'bun-plugin-isolated-decl';
 import UnpluginTypia from '@ryoppippi/unplugin-typia/bun';
 
 const outdir = `dist`;
 
 await $`rm -rf ${outdir}`;
 
+const glob = new Glob('./src/*.ts');
+const entrypoints = await Array.fromAsync(glob.scan('.'));
+
 await Bun.build({
-	entrypoints: ['./src/index.ts'],
+	entrypoints,
 	target: 'node',
 	external: ['*'],
 	outdir,
 	naming: '[dir]/[name].mjs',
 	plugins: [
-		UnpluginTypia({ cache: false }),
+		UnpluginTypia({ cache: false, log: 'verbose' }),
+		isolatedDecl(),
 	],
 });
