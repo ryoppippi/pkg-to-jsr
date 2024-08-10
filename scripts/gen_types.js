6a2a13870e91a2c2385ee3538cce2893ca143640
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-10 22:19:47 +0100

    feat(script/*): ts -> js+jsdoc

--- scripts/gen_types.js
+++ scripts/gen_types.js
@@ -1,15 +1,17 @@
+#!/usr/bin/env -S node
+
 import { $ } from 'dax-sh';
 import { resolve } from 'pathe';
-import type { JSONSchema } from 'json-schema-to-typescript';
 import { compile } from 'json-schema-to-typescript';
 import { consola } from 'consola';
 
 const SCHEMA_URL = `https://jsr.io/schema/config-file.v1.json`;
-const EXPORT_PATH = $.path(resolve(__dirname, '../src/jsr.d.ts'));
+const EXPORT_PATH = $.path(resolve(import.meta.dirname, '../src/jsr.d.ts'));
 
-const json = await fetch(SCHEMA_URL).then(async res => res.json() as unknown);
+/** @type {import('json-schema-to-typescript').JSONSchema} */
+const json = await fetch(SCHEMA_URL).then(async res => res.json());
 
-const ts = await compile(json as JSONSchema, 'JSR');
+const ts = await compile(json, 'JSR');
 
 await $`echo ${ts} > ${EXPORT_PATH}`;
 
