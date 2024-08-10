9b05938e9379b52ba5d9e7228df80fb9e506d2c0
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-10 16:01:08 +0100

    chore(gen_types.ts): format

--- scripts/gen_types.ts
+++ scripts/gen_types.ts
@@ -1,14 +1,15 @@
-import { $ } from 'bun'
-import { resolve } from 'pathe'
+import { $ } from 'bun';
+import { resolve } from 'pathe';
+import type { JSONSchema } from 'json-schema-to-typescript';
 import { compile } from 'json-schema-to-typescript';
 import { consola } from 'consola';
 
 const SCHEMA_URL = `https://jsr.io/schema/config-file.v1.json`;
 const EXPORT_PATH = resolve(__dirname, '../src/jsr.d.ts');
 
-const json = await fetch(SCHEMA_URL).then(res => res.json());
+const json = await fetch(SCHEMA_URL).then(async res => res.json() as unknown);
 
-const ts = await compile(json, 'JSR');
+const ts = await compile(json as JSONSchema, 'JSR');
 
 await $`echo ${ts} > ${EXPORT_PATH}`;
 
