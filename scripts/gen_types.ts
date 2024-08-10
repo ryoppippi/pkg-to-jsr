ed77511943e1a4d4b3171a56bf9a7fa28d70d4a4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-10 20:16:21 +0100

    feat: bun shell -> dax-sh

--- scripts/gen_types.ts
+++ scripts/gen_types.ts
@@ -1,11 +1,11 @@
-import { $ } from 'bun';
+import { $ } from 'dax-sh';
 import { resolve } from 'pathe';
 import type { JSONSchema } from 'json-schema-to-typescript';
 import { compile } from 'json-schema-to-typescript';
 import { consola } from 'consola';
 
 const SCHEMA_URL = `https://jsr.io/schema/config-file.v1.json`;
-const EXPORT_PATH = resolve(__dirname, '../src/jsr.d.ts');
+const EXPORT_PATH = $.path(resolve(__dirname, '../src/jsr.d.ts'));
 
 const json = await fetch(SCHEMA_URL).then(async res => res.json() as unknown);
 
@@ -13,4 +13,4 @@ const ts = await compile(json as JSONSchema, 'JSR');
 
 await $`echo ${ts} > ${EXPORT_PATH}`;
 
-consola.success(`Generated types at ${EXPORT_PATH}`);
+consola.success(`Generated types at ${EXPORT_PATH.toString()}`);
