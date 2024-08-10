dc2e8938b2fa25c1ddc9ad22c08ce03cbc363ac5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-10 19:16:41 +0100

    fix: include/eclude logic

--- src/utils.ts
+++ src/utils.ts
@@ -64,7 +64,7 @@ export function getInclude(pkgJSON: PackageJson): string[] | undefined {
 		return;
 	}
 
-	return files.filter(file => isStartWithExclamation(file));
+	return files.filter(file => !isStartWithExclamation(file));
 }
 
 /**
@@ -78,7 +78,7 @@ export function getExclude(pkgJSON: PackageJson): string[] | undefined {
 	}
 
 	return files
-		.filter(file => !isStartWithExclamation(file))
+		.filter(file => isStartWithExclamation(file))
 		.map((file) => {
 			if (file.startsWith('!')) {
 				return file.slice(1);
