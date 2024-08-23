10e19298076b1cea4c33ff0e417f687b0892ffcf
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-23 23:55:20 +0100

    feat(gen_types.js): replace dax-sh with fs for file operations
    This commit replaces the use of dax-sh with the native fs module for
    file operations in the gen_types.js script. This change improves
    compatibility and reduces dependencies. The EXPORT_PATH is now resolved
    directly using the resolve function from 'pathe'. The file writing
    operation is handled by fs.writeFile instead of a shell command.

--- scripts/gen_types.js
+++ scripts/gen_types.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env -S node
 
-import { $ } from 'dax-sh';
+import fs from 'node:fs/promises';
 import { resolve } from 'pathe';
 import { compile } from 'json-schema-to-typescript';
 import { consola } from 'consola';
 
 const SCHEMA_URL = `https://jsr.io/schema/config-file.v1.json`;
-const EXPORT_PATH = $.path(resolve(import.meta.dirname, '../src/jsr.d.ts'));
+const EXPORT_PATH = resolve(import.meta.dirname, '../src/jsr.d.ts');
 
 /** @type {import('json-schema-to-typescript').JSONSchema} */
 const json = await fetch(SCHEMA_URL).then(async res => res.json());
 
 const ts = await compile(json, 'JSR');
 
-await $`echo ${ts} > ${EXPORT_PATH}`;
+await fs.writeFile(EXPORT_PATH, ts);
 
 consola.success(`Generated types at ${EXPORT_PATH.toString()}`);
