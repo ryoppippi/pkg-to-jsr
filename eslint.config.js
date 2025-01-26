1980283aebd364bfe3e145a6f74d3a5f04bba663
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-26 19:53:10 +0000

    chore: remove tsdown

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import { ryoppippi } from '@ryoppippi/eslint-config';
 export default ryoppippi({
 	type: 'lib',
 	svelte: false,
-	ignores: ['tsdown.config.ts', 'vite.config.ts', 'tests/**/*.json'],
+	ignores: ['vite.config.ts', 'tests/**/*.json'],
 	typescript: {
 		tsconfigPath: './tsconfig.json',
 		overrides: {
