310c7d1323f59a9e5e0290e39bae20a22de5cce8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-26 22:51:26 +0000

    feat(build): add external dependencies to build config
    - Import package.json to access dependencies
    - Add external dependencies to Bun build configuration

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import UnpluginTypia from '@ryoppippi/unplugin-typia/bun';
 import isolatedDecl from 'bun-plugin-isolated-decl';
+import pj from '../package.json';
 
 function relativePath(p: string): string {
 	const { dir } = import.meta;
@@ -12,6 +13,7 @@ await Bun.build({
 	outdir: relativePath('../dist'),
 	target: 'node',
 	minify: true,
+	external: Object.keys(pj.dependencies),
 	plugins: [
 		UnpluginTypia({ cache: false }),
 		isolatedDecl(),
