ee92685a908a21f278becbd944e0fee0ec7fd84f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-26 23:24:10 +0000

    feat(build): add cleanup step before build
    - Added `rm` from 'node:fs/promises' to remove the output directory
      before starting the build process.
    - Wrapped the build process in a condition to check if the script is
      the main module.

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,24 +1,32 @@
+import { rm } from 'node:fs/promises';
 import path from 'node:path';
 import UnpluginTypia from '@ryoppippi/unplugin-typia/bun';
 import isolatedDecl from 'bun-plugin-isolated-decl';
+
 import pj from '../package.json';
+
+const outdir = relativePath('../dist');
 
 function relativePath(p: string): string {
 	const { dir } = import.meta;
 	return path.resolve(dir, p);
 }
 
-await Bun.build({
-	entrypoints: [
-		relativePath('../src/index.ts'),
-		relativePath('../src/cli.ts'),
-	],
-	outdir: relativePath('../dist'),
-	target: 'node',
-	minify: true,
-	external: Object.keys(pj.dependencies),
-	plugins: [
-		UnpluginTypia({ cache: false }),
-		isolatedDecl(),
-	],
-});
+if (import.meta.main) {
+	await rm(outdir, { recursive: true, force: true });
+
+	await Bun.build({
+		entrypoints: [
+			relativePath('../src/index.ts'),
+			relativePath('../src/cli.ts'),
+		],
+		outdir,
+		target: 'node',
+		minify: true,
+		external: Object.keys(pj.dependencies),
+		plugins: [
+			UnpluginTypia({ cache: false }),
+			isolatedDecl(),
+		],
+	});
+}
