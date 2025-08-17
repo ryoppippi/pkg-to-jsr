ed7a929a970292ff945969e265c5f0cf059b010d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-08-17 13:44:15 +0100

    feat!: remove usage as a package

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,20 +2,14 @@ import { $ } from 'bun'
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-	entry: 'src/**/*.ts',
+	entry: 'src/cli.ts',
 	outDir: 'dist',
 	format: 'esm',
 	minify: true,
-	dts: true,
 	clean: true,
 	publint: true,
 	unused: true,
-  nodeProtocol: true,
-	external: [
-		'gunshi',
-		'consola',
-		'terminal-link',
-	],
+	nodeProtocol: true,
 	hooks: {
 		'build:before': async () => {
 			await $`bun ./scripts/gen_types.js`
